9bd889d10284f880cb15b94831f4c4d625372a0b
adeshinafuad <adeshinafuad@gmail.com>
2022-07-20 16:29:06 +0100

    adding vote choices

--- post/models.py
+++ post/models.py
@@ -1,3 +1,4 @@
+from ast import mod
 from django.db import models
 # from django.contrib.auth.models import User
 from accounts.models import CustomUser
@@ -21,6 +22,17 @@ class Post(models.Model):
         
 class Vote(models.Model):
     voter = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)  
+    VOTE_CHOICES = (
+        ("upvote","upvote"),
+        ("neutral","neutral"),
+        ("downvote","downvote"),
+    )
+    vote = models.CharField(max_length=8,choices=VOTE_CHOICES, default="neutal")
+    
+
+    
+    
+    
 
     
